9b3b6f12321997a75006646bdff63ba445041541
Syed Faheem <syedfaheem@Syeds-Laptop.local>
2024-12-18 08:41:02 -0500

    Added TaskQueue and few starter methods

--- TaskQueue.java
+++ TaskQueue.java
@@ -1,2 +1,64 @@
+import java.util.*;
+
 public class TaskQueue {
+
+    private Map<String, Task> taskMap = new HashMap<>();
+    private Map<String, Set<String>> taskDependencies = new HashMap<>();
+    private Map<String, Integer> taskExecutionOrder = new HashMap<>();
+    private List<String> nodes = new ArrayList<>();
+    private Map<String, List<String>> nodeAssignments = new HashMap<>();
+    private int taskCounter = 0;
+
+    public void addTask(String taskId, int priority, List<String> dependencies){
+        if(taskMap.containsKey(taskId)){
+            System.out.println("Task with ID " + taskId + " already exists.");
+            return;
+        }
+        Task task = new Task(taskId, priority, dependencies);
+        taskMap.put(taskId, task);
+        taskDependencies.put(taskId,  new HashSet<>(dependencies));
+        taskExecutionOrder.put(taskId, taskCounter++);
+    }
+
+    public void removeTask(String taskId){
+        if (!taskMap.containsKey(taskId)) {
+            System.out.println("Task with ID " + taskId + " does not exist.");
+            return;
+        }
+        taskMap.remove(taskId);
+        taskDependencies.remove(taskId);
+        taskExecutionOrder.remove(taskId);
+    }
+
+    public void updateTask(String taskId, int priority, List<String> dependencies){
+        if(!taskMap.containsKey(taskId)){
+            System.out.println("Task with ID " + taskId + " does not exist.");
+            return;
+        }
+        Task task = taskMap.get(taskId);
+        task.setPriority(priority);
+        task.setDependencies(dependencies);
+        taskDependencies.put(taskId, new HashSet<>(dependencies));
+    }
+
+    public List<String> getExecutionOrder(){
+
+    }
+
+    public void assignTasksToNodes(){
+
+    }
+
+    public void handleNodeFailures(String nodeId){
+
+    }
+
+    public void addNode(String nodeId){
+
+    }
+
+    public void removeNode(String nodeId){
+
+    }
+
 }
